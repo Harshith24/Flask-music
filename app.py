61fae5e21dadff86f6d0291b65457672de568094
Harshith Suresh <harshiths2004@gmail.com>
2023-07-25 22:16:04 -0700

    started adding layers to model

--- app.py
+++ app.py
@@ -1,22 +1,67 @@
 from flask import Flask, request, redirect, url_for, render_template, Response
-from camera import VideoCamera
+import cv2
+import numpy as np
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+from keras.layers import Input, Dense
 
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+# Input layer
+# print(camera.get(3), camera.get(4))
+# keras_input_layer = Input(shape=(int(camera.get(3)), int(camera.get(4))))
+
+# # First layer
+# keras_layer_1 = Dense(64)(keras_input_layer)
+
+# # Second layer
+# keras_layer_2 = Dense(32)(keras_layer_1)
+
+# #output
+# keras_output_layer = Dense(2)(keras_layer_2)
+
+#print('cv2.CAP_PROP_FRAME_WIDTH :', cv2.CAP_PROP_FRAME_WIDTH)   # 3
+#print('cv2.CAP_PROP_FRAME_HEIGHT:', cv2.CAP_PROP_FRAME_HEIGHT)  # 4
+#print('cv2.CAP_PROP_FPS         :', cv2.CAP_PROP_FPS)           # 5
+#print('cv2.CAP_PROP_FRAME_COUNT :', cv2.CAP_PROP_FRAME_COUNT)   # 7
+
+
+#model
+model = Sequential()
+model.add(Input(shape=(int(camera.get(4)), int(camera.get(3)), 3)))
+model.add(Dense(32))
+model.add(Dense(32))
+model.add(Dense(16))
+model.add(Dense(2))
+
+# model.summary()
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-def gen(camera):
+def gen_frames():  
     while True:
-        global df1
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            resized_frame = cv2.resize(frame, (640, 480))
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+            normalized_frame = resized_frame / 255.0
+            input_data = np.expand_dims(normalized_frame, axis=0)
+            # ppr_img = np.array(input_data)
+            result = model(input_data)
+            print(result)
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/success/<name>')
 def success(name):
