6a45072e6530fbcee711f9cf8b2e1b340277d15a
Harshith Suresh <harshiths2004@gmail.com>
2023-07-27 16:55:19 -0700

    training in progress. able to view web cam on flask along with appropriate emotion result from the model

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 from flask import Flask, request, redirect, url_for, render_template, Response
 import cv2
+import sys
 import numpy as np
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from PIL import Image
 
 app = Flask(__name__)
+
+faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 camera = cv2.VideoCapture(0)
-
-# Input layer
-# print(camera.get(3), camera.get(4))
-# keras_input_layer = Input(shape=(int(camera.get(3)), int(camera.get(4))))
-
-# # First layer
-# keras_layer_1 = Dense(64)(keras_input_layer)
-
-# # Second layer
-# keras_layer_2 = Dense(32)(keras_layer_1)
-
-# #output
-# keras_output_layer = Dense(2)(keras_layer_2)
 
 #print('cv2.CAP_PROP_FRAME_WIDTH :', cv2.CAP_PROP_FRAME_WIDTH)   # 3
 #print('cv2.CAP_PROP_FRAME_HEIGHT:', cv2.CAP_PROP_FRAME_HEIGHT)  # 4
@@ -29,11 +20,25 @@ camera = cv2.VideoCapture(0)
 
 #model
 model = Sequential()
-model.add(Input(shape=(int(camera.get(4)), int(camera.get(3)), 3)))
-model.add(Dense(32))
-model.add(Dense(32))
-model.add(Dense(16))
-model.add(Dense(2))
+# Convolutional Layers
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flatten the data for Dense layers
+model.add(Flatten())
+
+# Dense Layers
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+model.load_weights('emotion_recognition_model.h5')
+
+show_text=[0]
+emotion_dict = {0:"Angry",1:"Disgusted",2:"Fearful",3:"Happy",4:"Neutral",5:"Sad",6:"Surprised"}
 
 # model.summary()
 
@@ -47,17 +52,30 @@ def gen_frames():
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            resized_frame = cv2.resize(frame, (640, 480))
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            faces = faceCascade.detectMultiScale(gray_frame, 1.1, 5)
+            for (x,y,w,h) in faces:
+                # To draw a rectangle around the detected face  
+                cv2.rectangle(gray_frame,(x,y),(x+w,y+h),(0,255,255),2)
+                roi_gray_frame = gray_frame[y:y+h, x:x+w]	
+                # roi_gray_frame = gray[y:y + h, x:x + w]		    
+                # cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+                # cropped_img = cv2.cvtColor(roi_gray_frame, cv2.COLOR_BGR2GRAY)
+                cropped_img = cv2.resize(roi_gray_frame, (48, 48))
+                cropped_img = cropped_img / 255.0
+                cropped_img = np.expand_dims(cropped_img, axis=0)
+                result = model.predict(cropped_img)
+                maxindex = int(np.argmax(result))
+                emotion_label = emotion_dict[maxindex]
+                print(emotion_label)			    
+                show_text[0] = maxindex 
+                cv2.putText(gray_frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
             
-            normalized_frame = resized_frame / 255.0
-            input_data = np.expand_dims(normalized_frame, axis=0)
-            # ppr_img = np.array(input_data)
-            result = model(input_data)
-            print(result)
+
+            ret, buffer = cv2.imencode('.jpg', gray_frame) 
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
---
+++ train.py
@@ -0,0 +1,64 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
+from keras.optimizers import Adam
+import pandas as pd
+import numpy as np
+import cv2
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("./data/train.csv/train.csv")
+train_data = pd.read_csv("./data/test.csv/test.csv")
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+
+
+X_train = np.zeros(shape=(len(data), 48, 48))
+y_train = np.array(list(map(int, data['emotion'])))
+    
+for i, row in enumerate(data.index):
+    image = np.fromstring(data.loc[row, 'pixels'], dtype=int, sep=' ')
+    image = np.reshape(image, (48, 48))
+    X_train[i] = image
+
+X_train = np.array(X_train) / 255.0
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+y_train = np.array(y_train)
+
+for i in range(5, 7):
+    plt.imshow(X_train[i], cmap='gray')
+    plt.show()
+
+
+num_classes = 7
+y_train = to_categorical(y_train, num_classes)
+
+# Create the Sequential model
+model = Sequential()
+
+# Convolutional Layers
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flatten the data for Dense layers
+model.add(Flatten())
+
+# Dense Layers
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile the model
+model.compile(loss='categorical_crossentropy',optimizer="adam",metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, batch_size=64, epochs=50, validation_split=0.1)
+
+# Save the trained model for later use
+model.save('2emotion_recognition_model.h5')
